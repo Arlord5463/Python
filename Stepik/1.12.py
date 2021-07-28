0f9ba4f4003fb827cd728a4cb08097cf8cc4d65e
Arlord5463 <87767614+Arlord5463@users.noreply.github.com>
2021-07-28 18:01:45 +0300

    e
    e

---
+++ Stepik/1.12.py
@@ -0,0 +1,56 @@
+"""a = int(input())
+if (a <= 12 and a > -15) or (a > 14 and a < 17) or (a >= 19):
+    print(True)
+else:
+    print(False)"""
+
+
+
+'''' = float(input())
+c = float(input())
+b = str(input())
+if c == 0.0 and b == '/' or b == 'diw' or b == 'mod':
+    print('Деление на 0!')
+else:
+    if b == 'mod':
+        print(a % c)
+    elif b == 'pow':
+        print(a ** c)
+    elif b == 'diw':
+        print(a // c)
+    elif b == '/':
+        print(a / c)
+    elif b == '*':
+        print(a * c)
+    elif b == '+':
+        print(a + c)
+    elif b == '-':
+        print(a - c)'''
+
+'''a = str(input())
+
+if a == 'прямоугольник':
+    b = float(input())
+    c = float(input())
+    print(b*c)
+if a == 'треугольник':
+    a = float(input())
+    b = float(input())
+    c = float(input())
+    p = ((a + b + c) / 2)
+    S = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+    print(S)
+if a == 'круг':
+    r = float(input())
+    S = 3.14 * (r ** 2)
+    print(S)'''
+
+a = int(input())
+b = int(input())
+c = a * b
+while c > b and c > a:
+    if c % b == 0 and c % a == 0:
+
+
+
+
