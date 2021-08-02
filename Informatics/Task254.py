52adf4a60cde60ae5b2a50df24fc14d1d97027de
Arlord5463 <87767614+Arlord5463@users.noreply.github.com>
2021-08-02 15:21:16 +0300

    e

---
+++ Informatics/Task254.py
@@ -0,0 +1,8 @@
+line = int(input())
+column = int(input())
+line2 = int(input())
+column2 = int(input())
+if column == column2 or line == line2:
+    print('YES')
+else:
+    print('NO')
---
+++ Informatics/Task255 error.py
@@ -0,0 +1,8 @@
+line = int(input())
+column = int(input())
+line2 = int(input())
+column2 = int(input())
+if line - line2 == column - column2:
+    print('YES')
+else:
+    print('NO')
