9c5e6bd3dc11da0cb2f4cc54a61fb4a79652d88a
Arlord5463 <87767614+Arlord5463@users.noreply.github.com>
2022-02-21 14:03:14 +0300

    р
    рава

--- dd.py
+++ dd.py
@@ -1,21 +1,10 @@
-import pygame
-pygame.init()
+count = 0
+n = 1
+while n <= 2020:
+    d = 2020 % n
+    n += 1
+    if d == 22:
+        count += 1
+        print(n-1)
 
-WIDTH = 600
-HEIGHT = 600
-FPS = 60
-
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-
-running = True
-while running:
-    screen.fill(WHITE)
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            running = False
-    clock.tick(FPS)
-pygame.quit()
+print(count)
